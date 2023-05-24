ad6a089375813da304848fdec234c518cc31e46b
ambroisegithub <muhayimana21@gmail.com>
2023-05-24 08:29:16 +0200

    modified

---
+++ myappresto/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from . import views
+urlpatterns = [
+    path('menu-items/',views.MenuItemView.as_view()),
+    path('menu-items/<int:pk>',views.SingletMenuItemView.as_view()),
+]
---
+++ myappresto/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from rest_framework import generics
+from .models import MenuItem
+from .serializers import  MenuItemSerializer
+
+# Create your views here.
+
+class MenuItemView(generics.ListCreateAPIView):
+    
+    queryset = MenuItem.objects.all()
+    serializer_class = MenuItemSerializer
+    
+class SingletMenuItemView(generics.RetrieveUpdateAPIView,generics.DestroyAPIView):
+    
+    queryset = MenuItem.objects.all()
+    serializer_class = MenuItemSerializer    
